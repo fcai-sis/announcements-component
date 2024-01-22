14943bc84a35098ce4bf1525f6e1b33dc4304a39
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-01-22 16:05:51 +0200

    import and use user model from shared models

--- src/features/example/logic/handlers/example.handler.ts
+++ src/features/example/logic/handlers/example.handler.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
 
-import ExampleModel from "../../data/models/example.model";
+import { User } from '@fcai-sis/shared-models';
 
 /**
  * An example handler that creates an example document in the database
  */
 const handler = async (req: Request, res: Response) => {
-  const example = await ExampleModel.create({ message: req.params.message });
+  const example = await User.create({
+    name: 'test',
+    email: 'test@test.com',
+    password: 'test',
+  });
 
   res.status(200).json({ example });
 };
