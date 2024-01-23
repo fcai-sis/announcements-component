c3ad01dce287d04a9f5451a363e8fe931255f223
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-01-23 07:56:32 +0200

    implement create announcement logic

--- src/features/announcements/logic/handlers/createAnnouncement.handler.ts
+++ src/features/announcements/logic/handlers/createAnnouncement.handler.ts
@@ -1,16 +1,37 @@
+import mongoose from "mongoose";
 import { Request, Response } from "express";
+
+import AnnouncementModel, { AnnouncementSeverity } from "../../data/models/announcement.model";
 
 type HandlerRequest = Request<
   {},
   {},
-  {}
+  {
+    title: string;
+    content: string;
+    severity: AnnouncementSeverity;
+  }
 >;
 
 /*
  * Creates an announcement.
  * */
 const handler = async (req: HandlerRequest, res: Response) => {
+  const { title, content, severity } = req.body;
+  const authorId = new mongoose.Types.ObjectId();
 
+  const announcement = new AnnouncementModel({
+    title,
+    content,
+    severity,
+    authorId,
+  });
+
+  await announcement.save();
+
+  return res.json({
+    announcement,
+  });
 }
 
 const createAnnouncementHandler = handler;
