3ff7b05019ee73a043f7d9f0fb8133d8cb8eb84b
Amr Khaled <lewolfje@gmail.com>
2024-01-23 12:24:53 +0200

    Add announcement read, delete, and archive routes

--- src/features/announcements/logic/announcements.routes.ts
+++ src/features/announcements/logic/announcements.routes.ts
@@ -3,14 +3,32 @@ import { Router } from "express";
 import asyncHandler from "../../../core/asyncHandler";
 import createAnnouncementHandler from "./handlers/createAnnouncement.handler";
 import validateCreateAnnouncementRequestBodyMiddleware from "./middlewares/validateCreateAccouncementRequestBody.middleware";
+import readAnnouncementHandler from "./handlers/readannouncement.handler";
+import deleteAnnouncementHandler from "./handlers/deleteannouncement.handler";
+import archiveAnnouncementHandler from "./handlers/archiveannouncement.handler";
 
 export default (router: Router) => {
   router.post(
     "/create",
 
-    // Validate request body 
+    // Validate request body
     validateCreateAnnouncementRequestBodyMiddleware,
 
-    asyncHandler(createAnnouncementHandler),
+    asyncHandler(createAnnouncementHandler)
+  );
+  router.get(
+    "/read",
+
+    asyncHandler(readAnnouncementHandler)
+  );
+  router.delete(
+    "/delete",
+
+    asyncHandler(deleteAnnouncementHandler)
+  );
+  router.put(
+    "/archive",
+
+    asyncHandler(archiveAnnouncementHandler)
   );
 };
