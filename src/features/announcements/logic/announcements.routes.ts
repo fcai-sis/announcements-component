7e18ecaca8ac712d89a5a90fdf3d77a324f894ff
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-01-23 07:57:29 +0200

    wire up announcement creation stuff in router

--- src/features/announcements/logic/announcements.routes.ts
+++ src/features/announcements/logic/announcements.routes.ts
@@ -2,10 +2,15 @@ import { Router } from "express";
 import asyncHandler from "core/asyncHandler";
 
 import createAnnouncementHandler from "./handlers/createAnnouncement.handler";
+import validateCreateAnnouncementRequestBodyMiddleware from "./middlewares/validateCreateAccouncementRequestBody.middleware";
 
 export default (router: Router) => {
   router.post(
     "/create",
+
+    // Validate request body 
+    validateCreateAnnouncementRequestBodyMiddleware,
+
     asyncHandler(createAnnouncementHandler),
   );
 };
