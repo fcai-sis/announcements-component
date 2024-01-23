ed04dc80dedb955a26e543f74498ad0d4fcb7067
Amr Khaled <lewolfje@gmail.com>
2024-01-23 19:06:41 +0200

    Fix import typos and add validation middleware

--- src/features/announcements/logic/announcements.routes.ts
+++ src/features/announcements/logic/announcements.routes.ts
@@ -3,9 +3,11 @@ import { Router } from "express";
 import asyncHandler from "../../../core/asyncHandler";
 import createAnnouncementHandler from "./handlers/createAnnouncement.handler";
 import validateCreateAnnouncementRequestBodyMiddleware from "./middlewares/validateCreateAccouncementRequestBody.middleware";
-import readAnnouncementHandler from "./handlers/readannouncement.handler";
-import deleteAnnouncementHandler from "./handlers/deleteannouncement.handler";
-import archiveAnnouncementHandler from "./handlers/archiveannouncement.handler";
+import readAnnouncementHandler from "./handlers/readAnnouncement.handler";
+import deleteAnnouncementHandler from "./handlers/deleteAnnouncement.handler";
+import archiveAnnouncementHandler from "./handlers/archiveAnnouncement.handler";
+import validatePaginationQueryParams from "./middlewares/validatePaginationQueryParams.middleware";
+import validateDeleteArchiveAnnouncementRequestBodyMiddleware from "./middlewares/validateDeleteAnnouncement.middleware";
 
 export default (router: Router) => {
   router.post(
@@ -18,17 +20,17 @@ export default (router: Router) => {
   );
   router.get(
     "/read",
-
+    validatePaginationQueryParams,
     asyncHandler(readAnnouncementHandler)
   );
   router.delete(
     "/delete",
-
+    validateDeleteArchiveAnnouncementRequestBodyMiddleware,
     asyncHandler(deleteAnnouncementHandler)
   );
   router.put(
     "/archive",
-
+    validateDeleteArchiveAnnouncementRequestBodyMiddleware,
     asyncHandler(archiveAnnouncementHandler)
   );
 };
