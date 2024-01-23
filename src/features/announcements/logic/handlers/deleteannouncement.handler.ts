de4a1b64dc01f28e7dae907dae7f2124499d4994
Amr Khaled <lewolfje@gmail.com>
2024-01-23 19:07:11 +0200

    Refactor delete announcement handler to handle missing announcement ID

--- src/features/announcements/logic/handlers/deleteannouncement.handler.ts
+++ src/features/announcements/logic/handlers/deleteannouncement.handler.ts
@@ -15,15 +15,15 @@ type HandlerRequest = Request<
 const handler = async (req: HandlerRequest, res: Response) => {
   const announcement = req.body.id;
 
-  if (!announcement) {
-    return res
-      .status(400)
-      .json({ error: "Announcement ID is required in the request body" });
-  }
-
   const deletedAnnouncement = await AnnouncementModel.findByIdAndDelete(
     announcement
   );
+
+  if (!deletedAnnouncement) {
+    return res.status(404).send({
+      error: "Announcement not found",
+    });
+  }
 
   return res.status(200).send({
     data: deletedAnnouncement,
