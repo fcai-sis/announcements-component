2d3fea1dd612b844bf3a20de3e59e533a2515525
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-01-23 07:56:19 +0200

    extract severity into separate type

---
+++ src/features/announcements/data/models/announcement.model.ts
@@ -0,0 +1,48 @@
+import { userModelName } from "@fcai-sis/shared-models";
+import mongoose, { InferSchemaType, Schema } from "mongoose";
+
+export const announcementSeverities = ["info", "warning", "danger"] as const;
+export type AnnouncementSeverity = typeof announcementSeverities[number];
+
+const announcementSchema: Schema = new Schema<AnnouncementType>({
+  authorId: {
+    type: Schema.Types.ObjectId,
+    ref: userModelName,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+  },
+  severity: {
+    type: String,
+    enum: announcementSeverities,
+    required: true,
+  },
+  archived: {
+    type: Boolean,
+    default: false,
+  }
+});
+
+const announcementModelName = "Announcement";
+
+const AnnouncementModel = mongoose.model<AnnouncementType>(
+  announcementModelName,
+  announcementSchema
+);
+
+export default AnnouncementModel;
+
+type AnnouncementType = InferSchemaType<typeof announcementSchema>;
