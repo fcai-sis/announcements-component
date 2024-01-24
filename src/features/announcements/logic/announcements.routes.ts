07f1aa5d1b2597614f9991f0e390e4415d7f3b43
Youssef Atta'i <youssefgalalnazem@gmail.com>
2024-01-24 09:34:26 +0200

    use shared pagination middleware

--- src/features/announcements/logic/announcements.routes.ts
+++ src/features/announcements/logic/announcements.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 
 import { asyncHandler } from "@fcai-sis/shared-utilities";
+import { paginationQueryParamsMiddleware } from "@fcai-sis/shared-middlewares";
 
+import readAnnouncementHandler from "./handlers/readAnnouncements.handler";
 import createAnnouncementHandler from "./handlers/createAnnouncement.handler";
-import validateCreateAnnouncementRequestBodyMiddleware from "./middlewares/validateCreateAccouncementRequestBody.middleware";
-import readAnnouncementHandler from "./handlers/readannouncement.handler";
-import deleteAnnouncementHandler from "./handlers/deleteannouncement.handler";
-import archiveAnnouncementHandler from "./handlers/archiveannouncement.handler";
 import updateAnnouncementHandler from "./handlers/updateAnnouncement.handler";
+import deleteAnnouncementHandler from "./handlers/deleteAnnouncement.handler";
+import archiveAnnouncementHandler from "./handlers/archiveAnnouncements.handler";
 import updateAnnouncementValidator from "./middlewares/updateAnnouncementValidator.middleware";
+import ensureAnnouncementIdInParamsMiddleware from "./middlewares/ensureAnnouncementIdInParams.middleware";
+import validateCreateAnnouncementRequestBodyMiddleware from "./middlewares/validateCreateAccouncementRequestBody.middleware";
 
 export default (router: Router) => {
   router.post(
@@ -20,23 +22,33 @@ export default (router: Router) => {
     asyncHandler(createAnnouncementHandler)
   );
   router.get(
-    "/read",
+    "/",
+
+    // Pagination query params
+    paginationQueryParamsMiddleware,
 
     asyncHandler(readAnnouncementHandler)
   );
   router.delete(
-    "/delete",
+    "/delete/:announcementId",
+
+    ensureAnnouncementIdInParamsMiddleware,
 
     asyncHandler(deleteAnnouncementHandler)
   );
   router.put(
     "/archive",
 
+    ensureAnnouncementIdInParamsMiddleware,
+
     asyncHandler(archiveAnnouncementHandler)
   );
   router.put(
-    "/update",
+    "/update/:announcementId",
+
+    ensureAnnouncementIdInParamsMiddleware,
     updateAnnouncementValidator,
+
     asyncHandler(updateAnnouncementHandler)
   );
 };
